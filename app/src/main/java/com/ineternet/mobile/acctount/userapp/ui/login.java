9ed2e63b49bf263fbf5823b43800b53d3083d448
dilky <dilky81@gmail.com>
2016-07-07 14:26:44 +0900

    최초등록

---
+++ app/src/main/java/com/ineternet/mobile/acctount/userapp/ui/login.java
@@ -0,0 +1,18 @@
+package com.ineternet.mobile.acctount.userapp.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ineternet.mobile.acctount.userapp.R;
+
+/**
+ * Created by user on 2016-07-07.
+ */
+public class login extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.login);
+    }
+}
